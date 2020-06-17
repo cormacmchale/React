0f299e07f6f2da07861208fef3d7caaa6613f7a8
unknown <cormacmchale1@gmail.com>
2020-06-17 12:49:00 +0100

    toolbar and navi added

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,15 +1,26 @@
+//Main React Library
 import React from 'react';
-import logo from './logo.svg';
+//css
 import './App.css';
 import Game from './Game';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Home';
+
+//router and toolbar (Naviagation and website skeleton)
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import MyToolbar from './Toolbar';
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Route path="/game" component={Game}/>
+    <div>
+      <MyToolbar></MyToolbar>
+      <div className="centerView">
+      <Router> 
+        <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/game" component={Game} />
+        </Switch>
       </Router>
+      </div>
     </div>
   );
 }
