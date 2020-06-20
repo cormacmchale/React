405e5b3e226b696ff8e2fe652a3353d88e03e44f
unknown <cormacmchale1@gmail.com>
2020-06-20 10:32:41 +0100

    added drawable canvas

--- firstapp/src/Canvas.js
+++ firstapp/src/Canvas.js
@@ -6,27 +6,74 @@ import './App.css';
 //return here to finish
 //https://itnext.io/using-react-hooks-with-canvas-f188d6e416c0
 
-class Canvas extends React.Component {
+//params for drawing
+const SCALE = 0.3
+const OFFSET = 80
+var flag = false;
 
-    // componentDidMount() {
-    //     const canvas = this.refs.canvas
-    //     const ctx = canvas.getContext("2d")
-    //     const img = this.refs.image
-    //     img.onload = () => {
-    //         ctx.drawImage(img, 0, 0)
-    //         ctx.font = "40px Courier"
-    //         ctx.fillText(this.props.text, 210, 75)
-    //     }
-    // }
-
-
-    render() {
-        return (
-            <div>
-                <canvas width={640} height={425} border="1rem solid #9c9c9c" onClick={e =>{alert(e.clientX)}}/>
-                {/* <img ref="image" src={cheese} className="hidden" /> */}
-            </div>
-        )
+//draw function
+function drawFlag(canvas, ctx, location) {
+    //allow continual drawing
+    flag = true;
+    console.log("flag is on");
+    ctx.beginPath()
+    ctx.fillStyle = "black"
+    //thickness
+    ctx.fillRect(location.x - 135, location.y - 115, 10, 10)
+    ctx.closePath()
+    ctx.restore()
+}
+//draw for mouse over
+function draw(canvas, ctx, location) {
+    console.log(flag+" moving")
+    //draw if been flagged by mouse down
+    if (flag == true) {
+        console.log("flag should be on");
+        ctx.beginPath()
+        ctx.fillStyle = "black"
+        //thickness
+        ctx.fillRect(location.x - 135, location.y - 115, 10, 10)
+        ctx.closePath()
+        ctx.restore()
     }
+}
+function stopDraw()
+{
+    console.log("reset flag");
+    flag = false;
+}
+
+//canavs component
+function Canvas() {
+    //initialise hook
+    const canvasRef = React.useRef(null)
+
+    return (
+        <div>
+            <canvas id="canvas" width={640} height={425} border="1rem solid #9c9c9c" ref={canvasRef}
+                //add in draw logic here
+                //on click
+                onMouseDown={e => {
+                    //get canvas and context on click
+                    const canvas = canvasRef.current
+                    const ctx = canvas.getContext('2d')
+                    //use the draw function from above
+                    drawFlag(canvas, ctx, { x: e.clientX, y: e.clientY })
+                }}
+                onMouseMove={e => {
+                    //get canvas and context on click
+                    const canvas = canvasRef.current
+                    const ctx = canvas.getContext('2d')
+                    //use the draw function from above
+                    draw(canvas, ctx, { x: e.clientX, y: e.clientY })
+                }}
+                onMouseUp={e => {
+                    stopDraw()
+                }}
+            />
+            {/* <img ref="image" src={cheese} className="hidden" /> */}
+        </div>
+    )
+
 }
 export default Canvas
